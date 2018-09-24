5627e9926c616af0fdc9b8808c5a415e6bdc4b00
apple <apple@apples-MacBook-Pro.local>
2018-09-24 11:18:20 +0100

    added graphs

--- enhancement.js
+++ enhancement.js
@@ -1,16 +1,17 @@
-;(function() {
+// ;(function() {
 
-    chrome.runtime.onInstalled.addListener(function() {
+//     chrome.runtime.onInstalled.addListener(function() {
 
-        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-            chrome.declarativeContent.onPageChanged.addRules([{
-              conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'www.reddit.com', schemes: ['https'], pathContains: 'FantasyPL' },
-              })
-              ],
-                  actions: [new chrome.declarativeContent.ShowPageAction()]
-            }]);
-          });
-      });
+//         chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//             chrome.declarativeContent.onPageChanged.addRules(
+//                 [{
+//               conditions: [new chrome.declarativeContent.PageStateMatcher({
+//                 pageUrl: {hostEquals: '<all_urls>'},
+//               })
+//               ],
+//                   actions: [new chrome.declarativeContent.ShowPageAction()]
+//             }]);
+//           });
+//       });
 
-})();
+// })();
--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
+;(function() {
+
+
+})();
+
+let playerUrl = 'https://fantasy.premierleague.com/drf/element-summary/';
 
 function getPlayerData(callback) {
   let xhr = new XMLHttpRequest();
@@ -15,7 +21,62 @@ function getPlayerData(callback) {
   xhr.send(null);
 }
 
+function getDetailedPlayerData(id) {
+  return new Promise(function(resolve, reject) {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", playerUrl + id, true);
+
+    xhr.onload = function() {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.responseText));
+        } else {
+              reject(Error(xhr.statusText));
+        }
+    }
+  };
+  xhr.send(null);
+  });
+
+}
 getPlayerData(parsePlayerData);
+
+function getTop5(players, atrribute) {
+  return players.sort((a,b) => b[atrribute] - a[atrribute] ).slice(0,5);
+}
+
+function parseDetailedPlayerData() {
+  return JSON.parse(this.responseText);
+}
+
+function getNextOppentent(data) {
+  return data.fixtures_summary[0].opponent_short_name;
+}
+
+function isHomeGame(data) {
+   return data.fixtures_summary[0].is_home;
+}
+
+function addPlayerToExpectedPointsList(player,p , nextOpp, isHome) {
+  let expectedList = document.getElementsByClassName('expected-points-list')[0];
+  let playerEle = document.createElement('dd');
+  playerEle.classList.add("percentage");
+  playerEle.classList.add("percentage-" +  Math.round(p));
+   
+  let text = document.createElement("span");
+  text.classList.add("text");
+  let t = document.createTextNode(player.name + " - " + player.expected_next);
+  let br = document.createElement("br");
+  let venue = isHome ? "(H)" : "(A)";
+  let n = document.createTextNode(nextOpp + " " + venue);
+  text.appendChild(t);
+  text.appendChild(br);
+  text.appendChild(n);
+  playerEle.appendChild(text);
+  expectedList.appendChild(playerEle);
+
+}
 
 function parsePlayerData() {
   let result = JSON.parse(this.responseText);
@@ -25,29 +86,94 @@ function parsePlayerData() {
 
       return {
         name: player.web_name,
-        transfers_in_week: player.transfers_in_event
+        transfers_in_week: player.transfers_in_event,
+        transfers_out_week: player.transfers_out_event,
+        points_per_game: player.points_per_game,
+        expected_next: player.ep_next,
+        form: player.form,
+        id: player.id
       }
 
   });
 
   let sortedByMostTransfersIn = players.sort((a,b) => b.transfers_in_week - a.transfers_in_week ).slice(0,5);
-  let transfersInNameTableRow = document.getElementsByClassName('transfers-in-names')[0];
-  let transfersInAmountTableRow = document.getElementsByClassName('transfers-in-amount')[0];
+  let sortedByMostTransfersOut = players.sort((a,b) => b.transfers_out_week - a.transfers_out_week ).slice(0,5);
+  let mostPointsPerGame = players.sort((a,b) => b.points_per_game - a.points_per_game ).slice(0,5);
+  let top5PlayersHighestExpectedPoints = getTop5(players, 'expected_next');
+  let topExpectedPoints = top5PlayersHighestExpectedPoints.slice(0, 1)[0].expected_next;
 
-  sortedByMostTransfersIn.forEach(player => {
-      let nameData = createTableData(player.name);
-      transfersInNameTableRow.appendChild(nameData);
-
-      let amount = createTableData(player.transfers_in_week);
-      transfersInAmountTableRow.appendChild(amount);
+  top5PlayersHighestExpectedPoints.forEach(player => {
+    let p = (player.expected_next / topExpectedPoints) * 100;
+    console.log(p, topExpectedPoints)
+    let playerDataPromise = getDetailedPlayerData(player.id);
+    playerDataPromise.then(function(playerData) {
+      let nextOpp = getNextOppentent(playerData);
+      let isHome = isHomeGame(playerData);
+      addPlayerToExpectedPointsList(player,p, nextOpp, isHome);
+    });
   });
+
+
+
+
+  let transfersInNameTableRow = document.getElementsByClassName('transfers-in-names')[0];
+  let transfersOutNameTableRow = document.getElementsByClassName('transfers-out-names')[0];
+  let mostPointsPerGameDiv = document.getElementsByClassName('most-point-per-game')[0];
+
+  let highestAmount = sortedByMostTransfersOut.slice(0, 1)[0].transfers_out_week > sortedByMostTransfersIn.slice(0,1)[0].transfers_in_week ? sortedByMostTransfersOut.slice(0, 1)[0].transfers_out_week : sortedByMostTransfersIn.slice(0,1)[0].transfers_in_week;
+
+    sortedByMostTransfersIn.forEach(player => {
+      let percent = player.transfers_in_week / highestAmount;
+
+        let nameData = createPlayerData(player.name, player.transfers_in_week, percent);
+        transfersInNameTableRow.appendChild(nameData);
+    });
+
+    sortedByMostTransfersOut.forEach(player => {
+      let percent = player.transfers_out_week / highestAmount;
+      let nameData = createPlayerData(player.name, player.transfers_out_week, percent);
+
+      transfersOutNameTableRow.appendChild(nameData);
+      
+  });
+
+//   mostPointsPerGame.forEach(player => {
+//     let newTableData = document.createElement("li");
+//     let text = document.createTextNode(player.name + " - " + player.points_per_game);
+//     newTableData.appendChild(text);
+//     mostPointsPerGameDiv.appendChild(newTableData);
+// });
 }
 
-
-function createTableData(val) {
-  let newTableData = document.createElement("td");
+function createPlayerData(name, val, p) {
+  let newTableData = document.createElement("li");
   let text = document.createTextNode(val);
+  var br = document.createElement("br");
+
+  let playerSpanEle = document.createElement("span");
+  let nameText = document.createTextNode(name);
+  playerSpanEle.appendChild(nameText);
+
   newTableData.appendChild(text);
+  newTableData.appendChild(br);
+  newTableData.appendChild(playerSpanEle);
+  newTableData.classList.add("player");
+  
+
+  let size = 200 * p;
+  let s = size.toString() + "px";
+
+  newTableData.style.width = s;
+  newTableData.style.height = s;
+
+  let fontSize = 3 * p;
+  newTableData.style.fontSize = fontSize + 'rem';
+  playerSpanEle.style.fontSize = (fontSize * 0.5) + 'rem';
+
+  newTableData.style.color = '#fff';
+
+  let borderSize = 4 * p;
+  newTableData.style.borderWidth = borderSize + 'px';
 
   return newTableData;
 }
